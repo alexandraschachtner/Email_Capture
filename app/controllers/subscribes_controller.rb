b444b59eef5d3ef7c4688b6aca7edf436bd8d8e5
Alexandra <alexandra.schachtner@gmail.com>
2019-03-08 17:07:30 -0600

    radio buttons added, working on requiring radio buttons to submit

--- app/controllers/subscribes_controller.rb
+++ app/controllers/subscribes_controller.rb
@@ -1,17 +1,19 @@
 class SubscribesController < ApplicationController
+
+
 	def create
 
 		@subscribe = Subscribe.new subscribe_params
-		if @subscribe.save
-		redirect_to root_path, notice: "Hey Thanks"
-		else
-		redirect_to root_path, notice: "Nooooooo!"
+			if @subscribe.save
+				redirect_to root_path, notice: "Hey Thanks"
+			else
+				redirect_to root_path, notice: "Nooooooo!"
 	  end
 	end
 
 private
 
 	def subscribe_params
-		params.require(:subscribe).permit(:name, :email)
+		params.require(:subscribe).permit(:sub, :email)
 	end
 end
