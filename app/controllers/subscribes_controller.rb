020fceeeec4195936852041e9ba6e0d193edb7a3
Alexandra <alexandra.schachtner@gmail.com>
2019-03-12 19:21:04 -0500

    route file magically missing

--- app/controllers/subscribes_controller.rb
+++ app/controllers/subscribes_controller.rb
@@ -2,18 +2,25 @@ class SubscribesController < ApplicationController
 
 
 	def create
-
 		@subscribe = Subscribe.new subscribe_params
 			if @subscribe.save
 				redirect_to root_path, notice: "Hey Thanks"
 			else
 				redirect_to root_path, notice: "Nooooooo!"
-	  end
 	end
+
+def manage
+	
+end
+
+end
 
 private
 
 	def subscribe_params
-		params.require(:subscribe).permit(:sub, :email)
+		params.require(:subscribe).permit(:email)
 	end
+	# def sub_y?
+  #   	redirect_to root_path, notice: "ooopies daisy!"
+	# 	end
 end
